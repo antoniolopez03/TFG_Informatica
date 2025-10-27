d5fa031f071f12cc5636b0d0be8bc7583736a97e
antoniolopez03 <antonl11@ucm.es>
2025-10-27 17:39:12 +0100

    modelo_propio_2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.tfg"
+    namespace = "com.example.tfg_3"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"  
     
@@ -20,7 +20,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.tfg"
+        applicationId = "com.example.tfg_3"
         // minSdk = flutter.minSdkVersion
         minSdk = 21 
         targetSdk = flutter.targetSdkVersion
